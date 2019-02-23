7554705031800b9c4d5e54b95803f857c80f18f5
Kevin Chiu <kchiu@paciolan.com>
2019-02-22 16:12:56 -0800

    added fields and layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
 
 class App extends Component {
+  constructor(props){
+      super(props)
+      this.state = {
+        name : 'Name',
+        address:'address',
+        phone:'xxx-xxx-xxxx',
+        company:'Company Name'
+      }
+  }
+
+  updateValue (e) {
+      var theNewValue = e.target.value;
+      this.setState({
+        name:theNewValue
+      })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+          <div className="row">
+            <div class="form-group">
+              Name:<input className="form-control" value={this.state.name} onChange={this.updateValue.bind(this)} placeholder={this.state.name}/>
+            </div>
+          </div>
+          <div className="row border border-primary rounded col-sm-4">
+              <div class="col-sm-12"><center><h2>{this.state.company}</h2></center></div>
+              <div class="col-sm-2"></div>
+              <div class="col-sm-10">{this.state.name}</div>
+              <div class="col-sm-2"></div>
+              <div class="col-sm-10">{this.state.address}</div>
+              <div class="col-sm-2"></div>
+              <div class="col-sm-10">{this.state.phone}</div>
+          </div>
       </div>
     );
   }
